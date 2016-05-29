db08cbc6558ed50627873a69a8e82208f0dd82ce
ashokmor <ashokmor@users.noreply.github.com>
2016-05-29 16:57:34 +0530

    Added more log

--- DockerGitHello.java
+++ DockerGitHello.java
@@ -1,6 +1,10 @@
 public class DockerGitHello {
 
   public static void main(String args[]) {
+    System.out.println(".....................");
     System.out.println("Hello from Docker, Github and CI autobuild !!!");
+    System.out.println("...............");
+    System.out.println("........");
+    System.out.println("....By Ashok Mor....");
   }
 }

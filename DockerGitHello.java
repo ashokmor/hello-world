36a8fcec012f81121d5cec0467f9b5652f1fd935
Ashok Mor <amor@asite.com>
2019-07-04 15:48:22 +0530

    Updated java source

--- DockerGitHello.java
+++ DockerGitHello.java
@@ -2,7 +2,7 @@ public class DockerGitHello {
 
   public static void main(String args[]) {
     System.out.println(".....................");
-    System.out.println("Hello from Docker, Github and CI autobuild !!!");
+    System.out.println("Hello from Docker, Github and CI autobuild, With Sanjay !!!");
     System.out.println("...............");
     System.out.println("........Time: "+ (new java.util.Date()));
   }
